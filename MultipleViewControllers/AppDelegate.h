56edf76ee6f6e1e83df0b195ac265e9fd940c753
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-01 16:55:56 -0700

    Initial Commit

---
+++ MultipleViewControllers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultipleViewControllers
+//
+//  Created by Anthony Dagati on 10/1/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultipleViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultipleViewControllers
+//
+//  Created by Anthony Dagati on 10/1/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
